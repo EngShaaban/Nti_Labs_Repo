14da870b84b10f87afdc158a3693b9af07900fa9
Shaban Abdullah Awad <shaaban.abdulla.abdelsalam@gmail.com>
2021-08-08 17:56:52 +0200

    Day#01 Lab#01

---
+++ Lab1.c
@@ -0,0 +1,55 @@
+/*************************************************************************************
+ * Title                 :   Lab1
+ * Filename              :   Lab1.c
+ * Author                :   Shaban Abdullah
+ * Origin Date           :   Aug 8, 2021
+ * Version               :   0.0.0
+ * Compiler              :   GCC
+ * Notes                 :   None.
+ *
+ *************************************************************************************/
+
+/*
+ * Include Standard input/output "stdio.h" to able to use printf and scanf functions.
+ */
+#include<stdio.h>
+
+// Calling the main function where the compilation starts.
+int main(void)
+{
+	// Print the massage using one printf() function.
+	printf("My name is\t: Shaban Abdullah\n"
+			"My birthday is  : 10 May 1989\n"
+			"I am undergraduate student\n"
+			"Graduation year is: 2022\n");
+
+
+	// This is a single line comment.
+
+	/*
+	 * This is
+	 * Multi-lines
+	 * comment.
+	 */
+
+
+	/********* Consider this code as comments. **********/
+
+	// METHODE 1.
+
+	/*printf("My name is\t: Shaban Abdullah\n");
+	printf("My birthday is  : 10 May 1989\n");
+	printf("I am undergraduate student\n");
+	printf("Graduation year is: 2022\n");   */
+
+
+	// METHODE 2.
+
+	//	printf("My name is\t: Shaban Abdullah\n");
+	//	printf("My birthday is  : 10 May 1989\n");
+	//	printf("I am undergraduate student\n");
+	//	printf("Graduation year is: 2022\n");
+
+}//End of main().
+
+
